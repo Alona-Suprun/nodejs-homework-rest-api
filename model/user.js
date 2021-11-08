8620967dc19acf65de11fa37871890db8439c2f3
Alona-Suprun <74992052+Alona-Suprun@users.noreply.github.com>
2021-11-08 17:59:52 +0200

    hw6

--- model/user.js
+++ model/user.js
@@ -1,16 +1,16 @@
 const { Schema, model } = require('mongoose');
+const gravatar = require('gravatar');
+const crypto = require('crypto');
+const { Subscription } = require('../config/constants');
 const bcrypt = require('bcryptjs');
+const { string } = require('joi');
 const SALT_FACTOR = 6;
 
 const userSchema = new Schema(
   {
-    name: {
-      type: String,
-      default: 'Guest',
-    },
     email: {
       type: String,
-      required: [true, 'Set email for user'],
+      required: [true, 'Email is required'],
       unique: true,
       validate(value) {
         const re = /\S+@\S+.\S+/;
@@ -19,17 +19,29 @@ const userSchema = new Schema(
     },
     password: {
       type: String,
-      required: [true, 'Set password for user'],
+      required: [true, 'Password is required'],
     },
     subscription: {
       type: String,
-      enum: ['starter', 'pro', 'business'],
-      default: 'starter',
+      enum: {
+        values: [Subscription.START, Subscription.PRO, Subscription.BUSINESS],
+        message: 'Subscription not allowed',
+      },
+      default: Subscription.START,
     },
     token: {
       type: String,
       default: null,
     },
+    avatar: {
+      type: String,
+      default: function () {
+        return gravatar.url(this.email, { s: '250' }, true);
+      },
+    },
+    idUserCloud: { type: String, default: null },
+    isVerified: { type: Boolean, default: false },
+    verifyToken: { type: String, required: true, default: crypto.randomUUID() },
   },
   {
     versionKey: false,
